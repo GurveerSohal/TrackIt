0a00da0a52e1ec14fe191640371d692c02e48832
Gurveer Sohal <gurveersohal433@gmail.com>
2023-06-16 12:22:25 +0530

    create an account and get all accounts

--- server/storage.go
+++ server/storage.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 
 	"github.com/google/uuid"
 	_ "github.com/lib/pq"
@@ -12,6 +13,7 @@ type Storage interface {
 	DeleteAccount(uuid.UUID) error
 	UpdateAccount(*Account) error
 	GetAccountByID(uuid.UUID) (*Account, error)
+	GetAccounts() ([]*Account, error)
 }
 
 type PostgresStore struct {
@@ -20,7 +22,7 @@ type PostgresStore struct {
 
 func NewPostgresStore() (*PostgresStore, error) {
 	// TO DO remove password from here
-	connStr := "user=postgres dbname=postgres password=trackit sslmode=disable"
+	connStr := "user=postgres dbname=trackitdb sslmode=disable"
 	db, err := sql.Open("postgres", connStr)
 
 	if err != nil {
@@ -42,17 +44,28 @@ func (s *PostgresStore) Init() error {
 }
 
 func (s *PostgresStore) createAccountTable() error {
-	query := `CREATE TABLE IF NOT EXISTS ACCOUNTS (
+	statement := `CREATE TABLE IF NOT EXISTS accounts (
 		ID uuid PRIMARY KEY NOT NULL DEFAULT gen_random_uuid(),
 		Username varchar(255),
 		Email varchar(255),
 		CreatedAt timestamp
 	);`
 
-	_, err := s.db.Exec(query)
+	_, err := s.db.Exec(statement)
 	return err
 }
 func (s *PostgresStore) CreateAccount(a *Account) error {
+	statement := `INSERT INTO accounts (Username, Email, CreatedAt) 
+			VALUES ($1, $2, $3)`
+
+	resp, err := s.db.Exec(statement, a.Username, a.Email, a.CreatedAt)
+
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("%+v\n", resp)
+
 	return nil
 }
 
@@ -66,4 +79,34 @@ func (s *PostgresStore) DeleteAccount(id uuid.UUID) error {
 
 func (s *PostgresStore) GetAccountByID(id uuid.UUID) (*Account, error) {
 	return &Account{}, nil
+}
+
+func (s *PostgresStore) GetAccounts() ([]*Account, error) {
+	query := `SELECT * FROM accounts`
+
+	rows, err := s.db.Query(query)
+
+	if err != nil {
+		return nil, err
+	}
+
+	accounts := []*Account{}
+
+	for rows.Next() {
+		account := new(Account)
+
+		err := rows.Scan(
+			&account.ID,
+			&account.Username,
+			&account.Email,
+			&account.CreatedAt)
+
+		if err != nil {
+			return nil, err
+		}
+
+		accounts = append(accounts, account)
+	}
+
+	return accounts, nil
 }
